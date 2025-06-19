0b538ea50552b00472dc7c2e650a5bb82cfe5f69
balloon <balloon@MAC21009.local>
2025-06-20 02:56:21 +0800

    create event article_view, article_read

---
+++ javascript/main.js
@@ -0,0 +1,26 @@
+window.dataLayer = window.dataLayer || [];
+
+function article_view(id, title) {
+	console.log("id: " + id + "\n" + "title: " + title);
+
+	// id 獨立送，預期會成為全域變數
+	dataLayer.push({
+		id: id
+	});
+
+	// title 跟著事件一起送，預期會成為區域變數
+	dataLayer.push({
+		event: "article_view",
+		title: title
+	});
+}
+
+function article_read(percentage) {
+	console.log("percentage: " + percentage);
+
+	// percentage 跟著事件一起送，預期會成為區域變數
+	dataLayer.push({
+		event: "article_read",
+		percentage: percentage
+	});
+}
