5d08bdc42f407d981ef42a0589623a87685c61cb
balloon <balloon@MAC21009.local>
2025-06-20 15:54:38 +0800

    use gtag

--- javascript/main.js
+++ javascript/main.js
@@ -1,15 +1,19 @@
 window.dataLayer = window.dataLayer || [];
 
+function gtag() {
+	window.dataLayer.push(arguments);
+}
+
 function article_view(id, title) {
 	console.log("id: " + id + "\n" + "title: " + title);
 
 	// id 獨立送，預期會成為全域變數
-	dataLayer.push({
+	gtag({
 		id: id
 	});
 
 	// title 跟著事件一起送，預期會成為區域變數
-	dataLayer.push('event', 'article_view', {
+	gtag('event', 'article_view', {
 		title: title
 	});
 }
@@ -18,7 +22,7 @@ function article_read(percentage) {
 	console.log("percentage: " + percentage);
 
 	// percentage 跟著事件一起送，預期會成為區域變數
-	dataLayer.push('event', 'article_read', {
+	gtag('event', 'article_read', {
 		percentage: percentage
 	});
 }
