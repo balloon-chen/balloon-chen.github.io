9d9f74726a8713670cbad1b19f6345d8abeccfa1
balloon <balloon@MAC21009.local>
2025-06-21 17:00:08 +0800

    install pure ga in pure_ga.html

---
+++ javascript/pure_ga.js
@@ -0,0 +1,22 @@
+function article_view(id, title) {
+	console.log("id: " + id + "\n" + "title: " + title);
+
+	// id 獨立送，預期會成為全域變數
+	gtag('set', {
+		id: id
+	});
+
+	// title 跟著事件一起送，預期會成為區域變數
+	gtag('event', 'article_view', {
+		title: title
+	});
+}
+
+function article_read(percentage) {
+	console.log("percentage: " + percentage);
+
+	// percentage 跟著事件一起送，預期會成為區域變數
+	gtag('event', 'article_read', {
+		percentage: percentage
+	});
+}
