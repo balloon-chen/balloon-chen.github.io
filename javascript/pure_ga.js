cf4f96c76d7f254893acda6794e4025d1fdf7f72
balloon <balloon@MAC21009.local>
2025-06-21 17:36:40 +0800

    remove gtag(config)

--- javascript/pure_ga.js
+++ javascript/pure_ga.js
@@ -1,17 +1,11 @@
-// 初始化 id = 0
-gtag('config', '<G-21E2TF2BFB>', {
-	id: 0
-});
-
 function article_view(id, title) {
 	console.log("id: " + id + "\n" + "title: " + title);
 
-	// id 獨立送，預期會成為全域變數
+	// id 獨立送，預期會成為全域參數
 	gtag('set', {
 		id: id
 	});
 
-	// title 跟著事件一起送，預期會成為區域變數
 	gtag('event', 'article_view', {
 		id, id,
 		title: title
@@ -21,6 +15,7 @@ function article_view(id, title) {
 function article_read(percentage) {
 	console.log("percentage: " + percentage);
 
+	// 若 article_read 先於 article_view 執行，則 id 為 undefined，只會送 percentage
 	gtag('get', 'G-21E2TF2BFB', 'id', (id) => {
 		gtag('event', 'article_read', {
 			id, id,
