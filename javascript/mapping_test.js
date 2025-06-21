8c4bce7f6a25f0d9bdbaea6bca5e88c4ff6cf297
balloon <balloon@MAC21009.local>
2025-06-22 03:33:23 +0800

    Add mapping_test

---
+++ javascript/mapping_test.js
@@ -0,0 +1,16 @@
+function mapping_test_event(id, params) {
+	console.log("id: " + id + "\n" + "params: " + params);
+
+	if (id === 1) {
+		dataLayer.push({
+			event: 'mapping_test_event'+id,
+			title: params
+		})
+	} else {
+		dataLayer.push({
+			event: 'mapping_test_event'+id,
+			id: id,
+			percentage: params
+		})
+	}
+}
