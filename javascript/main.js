293131954afa4cf4907e7db97d786fe843c787b9
balloon <balloon@MAC21009.local>
2025-06-21 21:40:11 +0800

    change gtag to dataLayer.push

--- javascript/main.js
+++ javascript/main.js
@@ -1,28 +1,18 @@
-window.dataLayer = window.dataLayer || [];
-
-function gtag() {
-	window.dataLayer.push(arguments);
-}
-
 function article_view(id, title) {
 	console.log("id: " + id + "\n" + "title: " + title);
 
-	// id 獨立送，預期會成為全域變數
-	gtag('set', {
-		id: id
-	});
-
-	// title 跟著事件一起送，預期會成為區域變數
-	gtag('event', 'article_view', {
+	dataLayer.push({
+		event: 'article_view',
+		id: id,
 		title: title
-	});
+	})
 }
 
 function article_read(percentage) {
 	console.log("percentage: " + percentage);
 
-	// percentage 跟著事件一起送，預期會成為區域變數
-	gtag('event', 'article_read', {
+	dataLayer.push({
+		event: 'article_read',
 		percentage: percentage
-	});
+	})
 }
