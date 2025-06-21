d7f69a69b04a7f24f820d60119c3a79138009f4d
balloon <balloon@MAC21009.local>
2025-06-22 03:48:45 +0800

    Adjust id to same mapping_key

--- javascript/mapping_test.js
+++ javascript/mapping_test.js
@@ -1,16 +1,16 @@
 function mapping_test_event(id, params) {
-	console.log("id: " + id + "\n" + "params: " + params);
+	console.log("params: " + params);
 
 	if (id === 1) {
 		dataLayer.push({
 			event: 'mapping_test_event'+id,
-			id: id,
+			id: 'mapping_key',
 			title: params
 		})
 	} else {
 		dataLayer.push({
 			event: 'mapping_test_event'+id,
-			id: id,
+			id: 'mapping_key',
 			percentage: params
 		})
 	}
